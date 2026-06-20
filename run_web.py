fece552989ae217e04ee5b444c26c0b5df4fb4ea
sec1900 <xinyuwl666@gmail.com>
2026-06-21 04:05:13 +0800

    chore: 清理已删除的 run_web.py

--- run_web.py
+++
@@ -1,3 +0,0 @@
-from dotenv import load_dotenv; load_dotenv('E:/GraphPT/.env')
-import os; os.environ['GRAPHPT_TOOL_TIMEOUT'] = '120'
-import uvicorn; uvicorn.run('graphpt.web.app:web_app', host='127.0.0.1', port=8080)
