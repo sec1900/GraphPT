8087a93f9948dc30b15aeb5ce2dab597b0067e59
sec1900 <xinyuwl666@gmail.com>
2026-06-20 21:03:52 +0800

    fix: 超时kill后adapter解析部分结果 + 清理临时文件 + L2简化

--- run_web.py
+++ run_web.py
@@ -1,2 +1,2 @@
-import os; os.environ['GRAPHPT_TOOL_TIMEOUT'] = '30'
+import os; os.environ['GRAPHPT_TOOL_TIMEOUT'] = '120'
 import uvicorn; uvicorn.run('graphpt.web.app:web_app', host='127.0.0.1', port=8080)
